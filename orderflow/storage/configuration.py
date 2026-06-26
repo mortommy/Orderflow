762f45b6b950b9fe0b88ad646e471348c0118954
mortommy <tommaso.morello@gmail.com>
2026-06-26 22:35:00 +0200

    new trend following version

--- orderflow/storage/configuration.py
+++ orderflow/storage/configuration.py
@@ -1,7 +1,7 @@
 # ------------------ PostgreSQL ----------------------
 PG_USER = 'remora_user'
 PG_PWD = 'remora'
-PG_HOST = '100.87.5.16'
+PG_HOST = '192.168.1.100'
 PG_PORT = '5432'
 PG_DATABASE = 'remora'
 
